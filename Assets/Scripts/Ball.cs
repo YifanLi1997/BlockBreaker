2adfd16807d137ccd095bd70dc16abd76803aa5c
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2019-10-10 15:40:24 +0100

    general function

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+
+    Vector3 ballToPaddleVector;
+    bool hasStarted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballToPaddleVector = transform.position - paddle.transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            ClickToLaunchTheBall();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        transform.position = paddle.transform.position + ballToPaddleVector;
+    }
+
+    private void ClickToLaunchTheBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthInUnit = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    float mouseXPosInUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithMouse();
+    }
+
+    public void MoveWithMouse()
+    {
+        mouseXPosInUnit = Input.mousePosition.x / Screen.width * screenWidthInUnit;
+        transform.position = new Vector2(Mathf.Clamp(mouseXPosInUnit, minX, maxX), transform.position.y);
+    }
+}
